8af2e673d72364773eb737f94343315297d30bd6
AnkitaRaman <720427ar@gmail.com>
2022-11-22 14:42:58 +0530

    3 commit

--- serverless/member.js
+++ serverless/member.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const app1 = express();
 const app2 = express();
 
+app1.use(cors())
+app2.use(cors())
 
 const MEMBERS_TABLE = 'memberdetails';
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
@@ -38,25 +40,117 @@ console.log(params);
   }
 });
 
-app2.post("/members", async function (req, res) {
-  const { memberId, name } = req.body;
-  // if (typeof memberId !== "string") {
-  //   res.status(400).json({ error: '"memberId" must be a string' });
-  // } else if (typeof name !== "string") {
-  //   res.status(400).json({ error: '"name" must be a string' });
-  // }
-
+app1.delete("/members/:memberId", (req, res) => {
   const params = {
     TableName: MEMBERS_TABLE,
-    Item: {
-      Member_ID: memberId,
-      Member_Name: name,
+    Key: {
+      Member_ID: parseInt(req.params.memberId),
     },
+  };
+  try{
+    const { Item } = dynamoDbClient.delete(params).promise();
+    res
+    .status(200)
+    .json(Item);
+  } catch(e){
+    res.status(500).json({ error: "Could not retreive member" });
+  }
+});
+
+app2.get("/members", async (req, res) => {
+  try {
+    console.log('Inside get member list');
+    const params = {
+      TableName: MEMBERS_TABLE,
+    };
+    const  items  = await dynamoDbClient.scan(params).promise();
+    console.log(items.Items);
+    if (items.Items) {
+      console.log('ir',items.Items);
+      console.log(items.Items.sort((a, b) => b.Total_Exp - a.Total_Exp))
+      res.json(items.Items.sort((a, b) => b.Total_Exp - a.Total_Exp));
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find member with provided "memberId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive member" });
+  }
+});
+
+app2.post("/members", async function (req, res) {
+  console.log(req.body.Skillset.split(','));
+  if(req.body.Total_Exp<4){
+    res.status(400).send({error:'Experience too low'});
+    return;
+  }
+  if(req.body.Skillset.split(',').length<3) {
+    res.status(400).send({error:'Skill do not match'});
+    return;
+  }
+  if(req.body.Project_start_date > req.body.Project_start_date) {
+    res.status(400).send({error:'End date is less than start date'});
+    return;
+  }
+  if(req.body.Allocation_percentage > 100) {
+    res.status(400).send({error:'Please enter allocation in %'});
+    return;
+  }
+  req.body.Member_ID= Number(new Date());
+  const params = {
+    TableName: MEMBERS_TABLE,
+    Item: req.body
   };
 
   try {
     await dynamoDbClient.put(params).promise();
-    res.json({ memberId, name });
+    res.json(req.body);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create member" });
+  }
+});
+
+app2.put("/members", async function (req, res) {
+  console.log(req.body.Skillset.split(','));
+  if(req.body.Total_Exp<4){
+    res.status(400).send({error:'Experience too low'});
+    return;
+  }
+  if(req.body.Skillset.split(',').length<3) {
+    res.status(400).send({error:'Skill do not match'});
+    return;
+  }
+  if(req.body.Project_start_date > req.body.Project_start_date) {
+    res.status(400).send({error:'End date is less than start date'});
+    return;
+  }
+  if(req.body.Allocation_percentage > 100) {
+    res.status(400).send({error:'Please enter allocation in %'});
+    return;
+  }
+  let today = new Date();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  if(req.body.Project_end_date < date && req.body.Allocation_percentage!=0) {
+    res.status(400).send({error:'If project end date is lesser than the current date, then the allocation percentage must be updated as 0.'});
+    return;
+  }
+  
+  if(req.body.Project_end_date > date && req.body.Allocation_percentage!=100) {
+    res.status(400).send({error:'If the project end date is greater than the current date, then the allocation percentage must be 100%'});
+    return;
+  }
+  req.body.Member_ID = parseInt(req.body.Member_ID)
+  const params = {
+    TableName: MEMBERS_TABLE,
+    Item: req.body
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json(req.body);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Could not create member" });
--- serverless/task.js
+++ serverless/task.js
@@ -3,9 +3,13 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 
+var request=require('request');
+
 const app1 = express();
 const app2 = express();
 
+app1.use(cors())
+app2.use(cors())
 
 const TASKS_TABLE = 'taskdetails';
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
@@ -13,24 +17,37 @@ const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 app1.use(express.json());
 app2.use(express.json());
 
-app1.get("/tasks/:taskId", async function (req, res) {
+app1.get("/tasks/:MemberId", async function (req, res) {
   console.log('here')
-  const params = {
+  let params = {
     TableName: TASKS_TABLE,
     Key: {
-      Task_ID: parseInt(req.params.taskId),
+      Member_ID: parseInt(req.params.MemberId),
     },
   };
 console.log(params);
   try {
     const { Item } = await dynamoDbClient.get(params).promise();
-    if (Item) {
-      res.json(Item);
-    } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find task with provided "taskId"' });
-    }
+    console.log('Item',Item);
+
+    request.get(`https://cda6yduyd7.execute-api.us-east-1.amazonaws.com/members/${parseInt(req.params.MemberId)}`, async function(err,resps,body){
+        
+      let result = JSON.parse(resps.body)   
+      console.log('result',result);
+
+     let Item3 = Object.assign({}, result, Item);
+     console.log('Item3',Item3);
+ 
+     if (Item3) {
+       res.json(Item3);
+     } else {
+       res
+         .status(404)
+         .json({ error: 'Could not find task with provided memberID' });
+     }
+
+  });
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Could not retreive task" });
@@ -38,28 +55,36 @@ console.log(params);
 });
 
 app2.post("/tasks", async function (req, res) {
-  const { taskId, name } = req.body;
-  // if (typeof taskId !== "string") {
-  //   res.status(400).json({ error: '"taskId" must be a string' });
-  // } else if (typeof name !== "string") {
-  //   res.status(400).json({ error: '"name" must be a string' });
-  // }
-
   const params = {
     TableName: TASKS_TABLE,
-    Item: {
-      Task_ID: taskId,
-      Task_Name: name,
-    },
+    Item: req.body,
   };
 
-  try {
-    await dynamoDbClient.put(params).promise();
-    res.json({ taskId, name });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not create task" });
-  }
+  request.get('https://cda6yduyd7.execute-api.us-east-1.amazonaws.com/members', async function(err,resps,body){
+        
+        let result = JSON.parse(resps.body).filter(o1 => o1.Member_ID === req.body.Member_ID); 
+       console.log("rest",result.length) ;
+       if(result.length < 0) {
+        res.send("Member Id is missing unable to insert task");
+        return;
+       }
+       if(req.body.Task_End_Date < result.Project_end_date){
+        res.status(400).json({ error: "Task end date should be greater than project end date" });
+       }
+       if(req.body.Task_End_Date < req.body.Task_Start_Date){
+        res.status(400).json({ error: "Task end date should be greater than task start date" });
+       }
+       
+      try {
+        await dynamoDbClient.put(params).promise();
+        res.json(resps.body);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Could not create task" });
+      }
+
+    });
+
 });
 
 app1.use((req, res, next) => {
